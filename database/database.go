7f35c1d72a4faef3a3d6d01f6bd140bcc7aa64d8
Kristanto Liu <kristantoliu@gmail.com>
2024-05-12 11:51:56 +0700

    update db, update view maps, update pinpoints

--- database/database.go
+++ database/database.go
@@ -1,6 +1,10 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,9 +12,22 @@ import (
 var DB *gorm.DB
 
 func InitDatabase() {
-	dsn := "root:1234@tcp(127.0.0.1:3306)/test_miniproject?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err != nil {
+		panic("Failed to load .env file")
+	}
 
-	var err error
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%s&loc=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_CHARSET"),
+		os.Getenv("DB_PARSE_TIME"),
+		os.Getenv("DB_LOC"),
+	)
+
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database!")
--- usecase/pinpoint.go
+++ usecase/pinpoint.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"fmt"
+	"math"
 	"mini-project/repository"
 )
 
@@ -38,7 +40,23 @@ func (usecase *PinpointUseCase) GetPinpoint(id uint) (repository.Pinpoint, error
 	return pinpoint, nil
 }
 
+const locationThreshold = 0.5 // Threshold for location comparison
+func isCloseEnough(coord1, coord2, threshold float64) bool {
+	fmt.Println(coord1, coord2, threshold, math.Abs(coord1-coord2))
+	return math.Abs(coord1-coord2) <= threshold
+}
 func (usecase *PinpointUseCase) CreatePinpoint(userId uint, name, description string, latitude, longitude float64) (repository.Pinpoint, error) {
+	pinpoints, err := usecase.PinpointRepo.GetAll()
+	if err != nil {
+		return repository.Pinpoint{}, err
+	}
+	for _, pinpoint := range pinpoints {
+		fmt.Println(pinpoint.Latitude, latitude, pinpoint.Longitude, longitude)
+		if isCloseEnough(pinpoint.Latitude, latitude, locationThreshold) && isCloseEnough(pinpoint.Longitude, longitude, locationThreshold) {
+			return repository.Pinpoint{}, nil
+		}
+
+	}
 	pinpoint, err := usecase.PinpointRepo.Create(userId, name, description, latitude, longitude)
 	if err != nil {
 		return repository.Pinpoint{}, err
@@ -62,7 +80,6 @@ func (usecase *PinpointUseCase) CreatePinpoint(userId uint, name, description st
 	}
 	return pinpoint, nil
 }
-
 func (usecase *PinpointUseCase) UpdatePinpoint(id uint, name, description string, latitude, longitude float64) (repository.Pinpoint, error) {
 	pinpoint, err := usecase.PinpointRepo.Update(id, name, description, latitude, longitude)
 	if err != nil {
